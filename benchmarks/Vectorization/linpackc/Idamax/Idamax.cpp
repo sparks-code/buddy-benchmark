7788e7b145e85bd7e09d97e9d488f3db4dd01f9a
zhangyi <zhangyi216@mails.ucas.ac.cn>
2023-09-30 00:58:51 +0800

    add Idamax

---
+++ benchmarks/Vectorization/linpackc/Idamax/Idamax.cpp
@@ -0,0 +1,88 @@
+#include "Idamax.h"
+
+/*----------------------*/ 
+int idamax(int n,float dx[],int incx)
+
+/*
+     finds the index of element having max. absolute value.
+     jack dongarra, linpack, 3/11/78.
+*/
+{
+	float dmax;
+	int i, ix, itemp;
+
+	if( n < 1 ) return(-1);
+	if(n ==1 ) return(0);
+	if(incx != 1) {
+
+		/* code for increment not equal to 1 */
+
+		ix = 0;
+		dmax = fabs((double)dx[0]);
+		ix = ix + incx;
+		for (i = 1; i < n; i++) {
+			if(fabs((double)dx[ix]) > dmax)  {
+				itemp = i;
+				dmax = fabs((double)dx[ix]);
+			}
+			ix = ix + incx;
+		}
+	}
+	else {
+
+		/* code for increment equal to 1 */
+
+		itemp = 0;
+		dmax = fabs((double)dx[0]);
+		for (i = 1; i < n; i++) {
+			if(fabs((double)dx[i]) > dmax) {
+				itemp = i;
+				dmax = fabs((double)dx[i]);
+			}
+		}
+	}
+	return (itemp);
+}
+
+int idamax(int n,double dx[],int incx)
+
+/*
+     finds the index of element having max. absolute value.
+     jack dongarra, linpack, 3/11/78.
+*/
+{
+	double dmax;
+	int i, ix, itemp;
+
+	if( n < 1 ) return(-1);
+	if(n ==1 ) return(0);
+	if(incx != 1) {
+
+		/* code for increment not equal to 1 */
+
+		ix = 0;
+		dmax = fabs((double)dx[0]);
+		ix = ix + incx;
+		for (i = 1; i < n; i++) {
+			if(fabs((double)dx[ix]) > dmax)  {
+				itemp = i;
+				dmax = fabs((double)dx[ix]);
+			}
+			ix = ix + incx;
+		}
+	}
+	else {
+
+		/* code for increment equal to 1 */
+
+		itemp = 0;
+		dmax = fabs((double)dx[0]);
+		for (i = 1; i < n; i++) {
+			if(fabs((double)dx[i]) > dmax) {
+				itemp = i;
+				dmax = fabs((double)dx[i]);
+			}
+		}
+	}
+	return (itemp);
+}
---
+++ benchmarks/Vectorization/linpackc/Idamax/Idamax.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <math.h>
+
+int idamax(int n,float dx[],int incx);
+int idamax(int n,double dx[],int incx);
