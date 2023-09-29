3e531acb843f06afd3ef00ca421088c4bd92e459
zhangyi <zhangyi216@mails.ucas.ac.cn>
2023-09-30 01:00:21 +0800

    add dmxpy

---
+++ benchmarks/Vectorization/linpackc/Dmxpy/Dmxpy.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <math.h>
+void dmxpy (int n1, float y[], int n2, int ldm, float x[], float m[]);
+void dmxpy (int n1, double y[], int n2, int ldm, double x[], double m[]);
