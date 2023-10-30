7eb57b524aea893a1f1924be33ae28932d6d938c
zhangyi <zhangyi216@mails.ucas.ac.cn>
2023-10-30 20:27:23 +0800

    fix dgefa.h

--- benchmarks/Vectorization/linpackc/Dgefa/Dgefa.h
+++ benchmarks/Vectorization/linpackc/Dgefa/Dgefa.h
@@ -4,12 +4,12 @@
 #include <math.h>
 #include <stdio.h>
 
-int dgefa_ROLL_float_gcc(float a[], int lda, int n, int ipvt[], int *info);
-int dgefa_ROLL_double_gcc(double a[], int lda, int n, int ipvt[], int *info);
-int dgefa_UNROLL_float_gcc(float a[], int lda, int n, int ipvt[], int *info);
-int dgefa_UNROLL_double_gcc(double a[], int lda, int n, int ipvt[], int *info);
+void dgefa_ROLL_float_gcc(float a[], int lda, int n, int ipvt[], int *info);
+void dgefa_ROLL_double_gcc(double a[], int lda, int n, int ipvt[], int *info);
+void dgefa_UNROLL_float_gcc(float a[], int lda, int n, int ipvt[], int *info);
+void dgefa_UNROLL_double_gcc(double a[], int lda, int n, int ipvt[], int *info);
 
-int dgefa_ROLL_float_clang(float a[], int lda, int n, int ipvt[], int *info);
-int dgefa_ROLL_double_clang(double a[], int lda, int n, int ipvt[], int *info);
-int dgefa_UNROLL_float_clang(float a[], int lda, int n, int ipvt[], int *info);
-int dgefa_UNROLL_double_clang(double a[], int lda, int n, int ipvt[], int *info);
+void dgefa_ROLL_float_clang(float a[], int lda, int n, int ipvt[], int *info);
+void dgefa_ROLL_double_clang(double a[], int lda, int n, int ipvt[], int *info);
+void dgefa_UNROLL_float_clang(float a[], int lda, int n, int ipvt[], int *info);
+void dgefa_UNROLL_double_clang(double a[], int lda, int n, int ipvt[], int *info);
